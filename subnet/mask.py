6d753fa9b04d813374410f557cdac735269ecc52
car7oskdr <carloscvl7@yahoo.com.mx>
2022-02-05 21:07:29 -0600

    mod tem mask.py

--- subnet/mask.py
+++ subnet/mask.py
@@ -6,20 +6,22 @@ class KnowMask:
         """ to Do """
         self.list_ip = list_ip
 
+    def mask(self):
+        """docstring for mask to DP"""
+
+        type_class = " "
         # mascarade de red de la ip
-        system("clear")
-        print('*' * 80)
         ip_string = '.'.join(map(str, self.list_ip))
         if self.list_ip[0] >= 0 and self.list_ip[0] < 128:
-            print('\n\tLa IP',ip_string,'es clase A la mascara ',
-                  'de red es 255.0.0.0 /8\n')
+            type_class = f'\n\tLa IP {ip_string} es clase A la ',
+                         'mascara de red es 255.0.0.0 /8\n'
         elif self.list_ip < 192:
-            print('\n\tLa IP',ip_string,'es clase B la mascara ',
-                  'de red es 255.255.0.0 /16\n')
+            type_class = f'\n\tLa IP {ip_string} es clase B la ',
+                         'mascara de red es 255.255.0.0 /16\n'
         elif self.list_ip < 224:
-            print('\n\tLa IP',ip_string,'es clase C la mascara ',
-                  'de red es 255.255.255.0 /24\n')
+            type_class = f'\n\tLa IP {ip_string} es clase C la ',
+                         'mascara de red es 255.255.255.0 /24\n')
         else:
             pass
-        print('*' * 80)
-        
+
+        return type_class

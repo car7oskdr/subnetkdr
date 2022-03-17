7a4f61967807bea8e8e33adcb81f24a26a4aae9d
car7oskdr <carloscvl7@yahoo.com.mx>
2022-03-17 01:14:40 -0600

    add first lambda on subnet/validaIp.py 03/17/21

--- subnet/validaIp.py
+++ subnet/validaIp.py
@@ -1,52 +1,41 @@
 # metodo para validar la IP.
 class ValidarIp():
-    """docstring for ValidarIp
-        
-        requiere: Una lista con los cuatro octetos en formato string
-                  ['255','255','255','255']
-        retorna: Una lista con:
-                    pos[0] valor voleano dependiendo si la ip 
-                    ingresada es correcta,
-                    pos[1] si la ip es incorrecta retorna un valor
-                    entero que indica el error a mostrar por 
-                    errors/validaIp,
-                    si la ip es correcta retornara una lista con los
-                    octetos en formato type int [A, B, C, D]
+    """docstring for ValidarIp to DO
     """
         
     def __init__(self, str_ip):
         self.str_ip = str_ip
 
     def validation(self):
-        
-        lista_ip_int = []
+
+
         final_list = []
+        list_ip_int = []
 
-        # conversión de valores de la lista a enteros.
+        
         try:
-            print(self.str_ip)
-            for x in range(len(self.str_ip)):
-                lista_ip_int.append(int(self.str_ip[x]))
+            # convertion of values of the list to int
+            list_ip_int = list(map(lambda ip_int: int(ip_int), self.str_ip))
 
-            # comprobar que los octetos esten dentro de los rangos.
-            for x in range(len(lista_ip_int)):
-                if lista_ip_int[x] > 0 and lista_ip_int[x] < 256:
+            # check that the octets are within the ranges.
+            for x in range(len(list_ip_int)):
+                if list_ip_int[x] > 0 and list_ip_int[x] < 256:
                     pass
 
                 else: 
                     final_list.append(False)
-                    final_list.append(3)
+                    final_list.append(2)
 
-            if len(lista_ip_int) == 4:
+            # check taht the ip has 4 octetos.
+            if len(list_ip_int) == 4:
                 final_list.append(True)
-                final_list.append(lista_ip_int)
-
+                final_list.append(list_ip_int)
             else:
                 final_list.append(False)
-                final_list.append(1)
+                final_list.append(3)
 
         except ValueError:
             final_list.append(False)
-            final_list.append(2)
+            final_list.append(1)
 
         return final_list

8dc959f5e8544a2d68b21c03dd461b585a0757ac
car7oskdr <carloscvl7@yahoo.com.mx>
2022-03-19 15:27:22 -0600

    add filter to subnet/validaIp

--- subnet/validaIp.py
+++ subnet/validaIp.py
@@ -3,36 +3,31 @@ class ValidarIp():
     """docstring for ValidarIp to DO
     """
         
-    def __init__(self, str_ip):
+    def __init__(self, str_ip, final_list = []):
         self.str_ip = str_ip
+    
 
     def validation(self):
 
-
-        final_list = []
         list_ip_int = []
-
+        final_list = []
         
         try:
             # convertion of values of the list to int
             list_ip_int = list(map(lambda ip_int: int(ip_int), self.str_ip))
 
-            # check that the octets are within the ranges.
-            for x in range(len(list_ip_int)):
-                if list_ip_int[x] > 0 and list_ip_int[x] < 256:
-                    pass
-
-                else: 
-                    final_list.append(False)
-                    final_list.append(2)
-
             # check taht the ip has 4 octetos.
             if len(list_ip_int) == 4:
-                final_list.append(True)
-                final_list.append(list_ip_int)
+                # check that the octets are within the ranges.
+                if len(list(filter(lambda x: x > 0 and x < 256, list_ip_int), 2)) == 4:
+                    final_list.append(True)
+                    final_list.append(list_ip_int)
+                else:
+                    final_list.append(False)
+                    final_list.append(3)        
             else:
                 final_list.append(False)
-                final_list.append(3)
+                final_list.append(2)
 
         except ValueError:
             final_list.append(False)
